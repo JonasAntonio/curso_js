71492ce23669256d64db3d2caaa9b49dbabe8ed6
JonasAntonio <jayv.jonas@gmail.com>
2020-07-14 15:08:54 -0300

    curso js

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,11 @@
+var a = 3
+let b = 4
+
+var a = 30 // var permite que a variável seja redeclarada no mesmo escopo
+b = 40 // let não permite redeclaração da variável
+
+console.log(a, b)
+
+const c = 5
+// c = 50 // constantes não podem ter seu valor alterado
+console.log(c)
